f7a5683c0cd68578d565c3dccd79ed952f9c7efd
Tat-T <ring_87_87@mail.ru>
2025-03-20 11:19:13 +0300

    Задание 1 и 2

---
+++ src/Animal.java
@@ -0,0 +1,33 @@
+package image;
+
+public class Animal {
+    String name;
+    String characteristic;
+
+    public Animal(String name, String characteristic) {
+        this.name = name;
+        this.characteristic = characteristic;
+    }
+
+    public void describe() {
+        System.out.println(name + " - " + characteristic);
+    }
+static class Tiger extends Animal{
+    public Tiger(){
+        super("Тигр", "хищник");
+    }
+}
+static class Kangaroo extends Animal{
+    public Kangaroo(){
+        super("Кенгуру", "сумчатое");
+    }
+}
+static class Crocodile extends Animal {
+    public Crocodile(){
+        super("Крокодил", "рептилия");
+    }
+}
+}
+
+
+
